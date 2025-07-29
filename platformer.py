492c4cc56f25f8ea110ac577dfa98ef696cd4f18
Amy Thompson <amythompson0325@gmail.com>
2025-07-29 11:21:32 -0400

    added dirt blocks to display

--- platformer.py
+++ platformer.py
@@ -13,14 +13,49 @@ pygame.display.set_caption('Jump Cat')
 
 background_img = pygame.image.load('bg.png')
 scaled_bg = pygame.transform.scale(background_img, (500,500))
-#upload & scale game background image to fit window
+dirt_img = pygame.image.load('grassCenter.png')
+tile_size = 100 #5x5 grid
+scaled_dirt = pygame.transform.scale(dirt_img, (tile_size,tile_size))
+#upload & scale game images to fit window
+
+
+class Map():
+    def __init__ (self, data):
+        self.tile_list = []
+        row_count = 0
+        for row in data:
+            column_count = 0
+            for tile in row: 
+                if tile == 1:
+                    dirt_rect = scaled_dirt.get_rect() #turns into rectangle
+                    dirt_rect.x = column_count * tile_size
+                    dirt_rect.y = row_count * tile_size
+                    tile = (scaled_dirt, dirt_rect)
+                    self.tile_list.append(tile)
+                column_count += 1
+            row_count += 1
+#adds the tiles
+    def draw(self): 
+            for tile in self.tile_list:
+                screen.blit(tile[0], tile[1])    
+#displays the tiles
+map_data = [
+[1, 1, 1, 1, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 1, 1, 1, 1]
+]
+map = Map(map_data)
+#for creating the map
 
 gameRunning = True
 #sets game to running by default
 while gameRunning:
 #all that happens while game runs is in this while loop
     screen.blit(scaled_bg, (0,0))
-#adds background image to display
+    map.draw()
+#adds game graphics to display
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameRunning = False
